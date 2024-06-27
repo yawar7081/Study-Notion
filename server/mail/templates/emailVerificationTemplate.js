278583098bce1fa3ddcb5ee8c86960ad49ba05ee
MOHD YAWAR RAZA <yawar7081@gmail.com>
2024-06-27 23:37:33 +0530

    Update emailVerificationTemplate.js

--- server/mail/templates/emailVerificationTemplate.js
+++ server/mail/templates/emailVerificationTemplate.js
@@ -60,6 +60,30 @@ const otpTemplate = (otp) => {
 			.highlight {
 				font-weight: bold;
 			}
+   .mohammad{
+  font-size: 1rem;
+  text-transform: uppercase;
+  text-align: center;
+  line-height: 1;
+  width:250px;
+  background-color:#000405;
+  border-radius:20px;
+  color:white;
+  font-weight: 900;
+  padding:3px 2px;
+}
+
+.fancy {
+  @supports (background-clip: text) or (-webkit-background-clip: text) {
+    background-image: 
+      url("data:image/svg+xml,%3Csvg width='2250' height='900' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cg%3E%3Cpath fill='%2300A080' d='M0 0h2255v899H0z'/%3E%3Ccircle cx='366' cy='207' r='366' fill='%2300FDCF'/%3E%3Ccircle cx='1777.5' cy='318.5' r='477.5' fill='%2300FDCF'/%3E%3Ccircle cx='1215' cy='737' r='366' fill='%23008060'/%3E%3C/g%3E%3C/svg%3E%0A");
+    background-size: 110% auto;
+    background-position: center;
+    color: transparent;
+    -webkit-background-clip: text;
+    background-clip: text;
+  }
+}
 		</style>
 	
 	</head>
@@ -70,7 +94,13 @@ const otpTemplate = (otp) => {
 					src="https://i.ibb.co/7Xyj3PC/logo.png" alt="StudyNotion Logo"></a>
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
-				<p>Dear User,</p>
+   				<div>
+  <h2>I'm <span style="--i:0;" data-text="Developer.">Developer.</span>
+    <span style="--i:1;" data-text="Designer.">Designer.</span>
+    <span style="--i:2;" data-text="Creator.">Creator.</span>
+  </h2>
+</div>
+				<p>Dear User,<p class="mohammad">MOHAMMAD<span class="fancy"> YAWAR </span>RAZA</p> </p>
 				<p>Thank you for registering with StudyNotion. To complete your registration, please use the following OTP
 					(One-Time Password) to verify your account:</p>
 				<h2 class="highlight">${otp}</h2>
